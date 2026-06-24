3eaf534b1e02e29d018438a2372ad1dd2c6c0c24
Suporte-Biscoite <suporte@biscoite.com.br>
2026-06-24 17:03:05 -0300

    fix: MessageFloatButton adicionado ao MainLayout

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,19 +1,18 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from '../Navbar'; // Sobe um nível para sair de layouts e achar a Navbar
-import Footer from '../Footer'; // Sobe um nível para sair de layouts e achar o Footer
+import Navbar from '../Navbar';
+import Footer from '../Footer';
 import LiveFloatButton from '../LiveFloatButton';
+import MessageFloatButton from '../MessageFloatButton';
 
 export default function MainLayout() {
   return (
     <div className="min-h-screen bg-[#F8FAFC] flex flex-col font-sans">
       <Navbar />
-      
       <main className="flex-1 w-full max-w-[1440px] mx-auto p-8 pb-20">
-            <Outlet />
+        <Outlet />
       </main>
-
       <LiveFloatButton />
-
+      <MessageFloatButton />
       <Footer />
     </div>
   );
